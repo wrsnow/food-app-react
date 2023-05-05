5692aa9befd98b1d58d7c4cb1bade1dbb0a758ec
wrsnow <wesleyrsnow@gmail.com>
2023-05-05 00:27:32 -0300

    Finish restyling project

--- src/components/db/FastFoodJSON.ts
+++ src/components/db/FastFoodJSON.ts
@@ -10,13 +10,15 @@ export default [
     id: 2,
     name: "Chicken nuggets",
     price: 4.49,
-    image: "https://images.pexels.com/photos/1199960/pexels-photo-1199960.jpeg",
+    image:
+      "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2013/9/12/1/FN_Picky-Eaters-Chicken-Nuggets_s4x3.jpg.rend.hgtvcom.616.462.suffix/1383770571120.jpeg",
   },
   {
     id: 3,
     name: "French fries",
     price: 2.49,
-    image: "https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg",
+    image:
+      "https://static.toiimg.com/thumb/54659021.cms?imgsize=275086&width=800&height=800",
   },
   {
     id: 4,
@@ -29,36 +31,42 @@ export default [
     id: 5,
     name: "Hot dog",
     price: 1.99,
-    image: "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg",
+    image:
+      "https://www.thedailymeal.com/img/gallery/ranking-12-fast-food-hot-dogs-from-worst-to-best/intro-1675120855.jpg",
   },
   {
     id: 6,
     name: "Salad",
     price: 3.49,
-    image: "https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg",
+    image:
+      "https://cdn.loveandlemons.com/wp-content/uploads/2021/04/green-salad.jpg",
   },
   {
     id: 7,
     name: "Ice cream cone",
     price: 1.49,
-    image: "https://images.pexels.com/photos/3026813/pexels-photo-3026813.jpeg",
+    image:
+      "https://www.thespruceeats.com/thmb/t-BDihkPvpodAhMIVqdWw6RQ4vQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/IceCreamConeCupcakesHERO-ef1da5f5b4be42129e420ad1b8088b9b.jpg",
   },
   {
     id: 8,
     name: "Donut",
     price: 0.99,
-    image: "https://images.pexels.com/photos/3026809/pexels-photo-3026809.jpeg",
+    image:
+      "https://www.oetker.com.br/Recipe/Recipes/oetker.com.br/br-pt/dessert/image-thumb__134134__RecipeDetailsLightBox/donut.jpg",
   },
   {
     id: 9,
     name: "Coffee",
     price: 1.29,
-    image: "https://images.pexels.com/photos/3026808/pexels-photo-3026808.jpeg",
+    image:
+      "https://images.news18.com/ibnlive/uploads/2021/12/coffee-mug-16387975963x2.jpg?impolicy=website&width=510&height=356",
   },
   {
     id: 10,
-    name: "Soda",
-    price: 1.19,
-    image: "https://images.pexels.com/photos/3026810/pexels-photo-3026810.jpeg",
+    name: "Tacos",
+    price: 6.19,
+    image:
+      "https://images-gmi-pmc.edge-generalmills.com/e59f255c-7498-4b84-9c9d-e578bf5d88fc.jpg",
   },
 ];
